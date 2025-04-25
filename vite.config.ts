7f7f7ea1b811fc3daf36aa3e56ff34380948af8d
Ariane Costa <anne.de.abreu@gmail.com>
2025-04-25 09:03:25 -0300

    Adicionando configurações para exibição no GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-project/",  // Adicione esta linha
+  build: {
+    outDir: 'dist',  // Verifique se corresponde ao seu script 'deploy'
+  },
 })
