c69081efaaacf06ff2696159571ab6bd1db1f7e8
Ariane Costa <134095652+Ariane2023@users.noreply.github.com>
2025-04-25 14:10:49 +0000

    Segunda tentativa de exibir a logo

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -1,12 +1,13 @@
 // src/componentes/Header.tsx
 
-import styles from "./Header.module.css"; // vamos criar esse estilo também
+import styles from "./Header.module.css";
+import logo from './assets/logo.svg';
 
 export function Header() {
   return (
     <div className={styles.topBox}>
       <div className={styles.header}>
-        <img src="./src/assets/logo.svg" alt="Logo" className={styles.logo} />
+        <img src={logo} alt="logo" className={styles.logo} />
         <h1>Perfil <strong>Github</strong></h1>
       </div>
     </div>
