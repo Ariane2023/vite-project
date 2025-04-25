fe74c87492408b4c060b6c2a66a942bc33810d38
Ariane Costa <134095652+Ariane2023@users.noreply.github.com>
2025-04-25 13:00:31 +0000

    Alterando o caminho da logo

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <div className={styles.topBox}>
       <div className={styles.header}>
-        <img src="src/assets/logo.svg" alt="Logo" className={styles.logo} />
+        <img src="./src/assets/logo.svg" alt="Logo" className={styles.logo} />
         <h1>Perfil <strong>Github</strong></h1>
       </div>
     </div>
